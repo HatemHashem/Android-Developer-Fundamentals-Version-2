55d8cd12057d530d6907ece41e9d0dd24bb24e7e
HatemHashem <hatemsayed001@gmail.com>
2020-04-13 21:22:31 +0200

    add average rating,description,improve the UI

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.java
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -16,13 +17,21 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     private WeakReference<TextView> mBookTitle;
     private WeakReference<ImageView> mThumbnail;
     private WeakReference<Context> context;
+    private WeakReference<RatingBar> mRating;
+    private WeakReference<TextView> mdescription;
+    private WeakReference<TextView> mPublishedDate;
     private static final String TAG = "FetchBook";
 
-    public FetchBook(Context context, TextView authorName, TextView bookTitle, ImageView thumbnail) {
+
+    public FetchBook(Context context, TextView authorName, TextView bookTitle, ImageView thumbnail,
+                     RatingBar rating, TextView publishedDate,TextView description) {
         this.mAuthorName = new WeakReference<>(authorName);
         this.mBookTitle = new WeakReference<>(bookTitle);
         this.mThumbnail = new WeakReference<>(thumbnail);
         this.context = new WeakReference<>(context);
+        this.mRating = new WeakReference<>(rating);
+        this.mPublishedDate = new WeakReference<>(publishedDate);
+        this.mdescription=new WeakReference<>(description);
     }
 
     @Override
@@ -38,6 +47,9 @@ public class FetchBook extends AsyncTask<String, Void, String> {
 
             String authorName = null;
             String bookName = null;
+            String publishedDate = null;
+            String description = null;
+            double averageRating = 0;
             JSONObject jsonObject = new JSONObject(s);
             JSONArray arrayItem = jsonObject.getJSONArray("items");
             int i = 0;
@@ -47,16 +59,23 @@ public class FetchBook extends AsyncTask<String, Void, String> {
                 JSONObject volumeInfo = book.getJSONObject("volumeInfo");
                 JSONObject imageLinks = volumeInfo.getJSONObject("imageLinks");
                 String thumbnail = imageLinks.getString("thumbnail");
-                Log.d(TAG, "onPostExecute: "+thumbnail);
+                Log.d(TAG, "onPostExecute: " + thumbnail);
                 GlideApp.with(context.get()).load(thumbnail).into(mThumbnail.get());
                 bookName = volumeInfo.getString("title");
                 authorName = volumeInfo.getString("authors");
+                publishedDate = volumeInfo.getString("publishedDate");
+                averageRating = volumeInfo.getDouble("averageRating");
+                description = volumeInfo.getString("description");
+                System.out.println(publishedDate + "test" + averageRating);
                 i++;
 
             }
             if (authorName != null && bookName != null) {
                 mAuthorName.get().setText(authorName);
                 mBookTitle.get().setText(bookName);
+                mPublishedDate.get().setText(publishedDate);
+                mRating.get().setRating((float) averageRating);
+                mdescription.get().setText(description);
             } else {
                 mAuthorName.get().setText("error");
                 mBookTitle.get().setText("error");
--- WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.java
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView mBookTitle;
     private TextView mBookAuthor;
     private ImageView thumbnailImage;
+    private RatingBar ratingBar;
+    private TextView publishedDate;
+    private TextView description;
+    //log
     private static final String TAG = "MainActivity";
 
     @Override
@@ -27,36 +32,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBookInput = findViewById(R.id.bookinput_edittext);
-        mBookTitle=findViewById(R.id.book_title_textview);
-        mBookAuthor=findViewById(R.id.author_textview);
-        thumbnailImage=findViewById(R.id.thumbnail_imageview);
+        mBookTitle = findViewById(R.id.book_title_textview);
+        mBookAuthor = findViewById(R.id.author_textview);
+        thumbnailImage = findViewById(R.id.thumbnail_imageview);
+        ratingBar=findViewById(R.id.rate);
+        publishedDate=findViewById(R.id.published_date_textview);
+        description=findViewById(R.id.description_textview);
     }
 
     public void searchBooks(View view) {
-        String queryString=mBookInput.getText().toString();
-        Log.d(TAG, "searchBooks: "+queryString);
-        ConnectivityManager connectivityManager=(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo=null;
-        InputMethodManager inputManger=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        if(inputManger!=null){
-            inputManger.hideSoftInputFromWindow(view.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+        String queryString = mBookInput.getText().toString();
+        Log.d(TAG, "searchBooks: " + queryString);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        InputMethodManager inputManger = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManger != null) {
+            inputManger.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
-        if(connectivityManager!=null){
-            networkInfo=connectivityManager.getActiveNetworkInfo();
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
         }
-        if(networkInfo!=null&&networkInfo.isConnected()
-        &&queryString.length()!=0){
-            new FetchBook(getApplicationContext(),mBookAuthor,mBookTitle,thumbnailImage).execute(queryString);
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(getApplicationContext(), mBookAuthor, mBookTitle, thumbnailImage,ratingBar,publishedDate,description).execute(queryString);
             mBookAuthor.setText("");
             mBookTitle.setText(R.string.loading);
+            ratingBar.setVisibility(View.VISIBLE);
+            publishedDate.setVisibility(View.VISIBLE);
+            description.setVisibility(View.VISIBLE);
+            thumbnailImage.setVisibility(View.VISIBLE);
 
-        }else {
-            if(queryString.length()==0){
+        } else {
+            thumbnailImage.setVisibility(View.GONE);
+            ratingBar.setVisibility(View.GONE);
+            publishedDate.setVisibility(View.GONE);
+            description.setVisibility(View.GONE);
+
+            if (queryString.length() == 0) {
                 mBookAuthor.setText("");
                 mBookTitle.setText(R.string.book_not_found);
-            }else {
+            } else {
                 mBookAuthor.setText("");
                 mBookTitle.setText(R.string.no_internet);
+
             }
         }
 
